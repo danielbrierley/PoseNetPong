67efee83a7b85292d610478251a3c39f80e4fe0d
Daniel Brierley <daniel@thebrierleys.co.uk>
2022-12-11 00:24:15 +0000

    paddle intersection

--- sketch.js
+++ sketch.js
@@ -15,27 +15,76 @@ function setup() {
 }
 
 function draw() {
+  //update ball velocity
   ballX += ballXVel;
-  if (ballX < ballRad) {
-    ballX -= ballXVel;
-    ballXVel = -ballXVel;
-  }
-  if (ballX >= screenWidth-ballRad) {
-    ballX -= ballXVel;
-    ballXVel = -ballXVel;
-  }
-
-  
   ballY += ballYVel;
-  if (ballY < ballRad) {
+
+  //Bounds
+  if (ballX < ballRad) { //left
+    ballX -= ballXVel;
+    ballXVel = -ballXVel;
+  }
+  if (ballX >= screenWidth-ballRad) { //right
+    ballX -= ballXVel;
+    ballXVel = -ballXVel;
+  }
+  if (ballY < ballRad) { //top
     ballY -= ballYVel;
     ballYVel = -ballYVel;
   }
-  if (ballY >= screenHeight-ballRad) {
+  if (ballY >= screenHeight-ballRad) { //bottom
     ballY -= ballYVel;
     ballYVel = -ballYVel;
   }
 
+
+  if (Math.abs(ballYVel) > Math.abs(ballXVel)) {
+
+  }
+
+  ai = Math.atan(ballXVel/ballYVel)
+
+  //console.log(convDegrees(ai))
+  
+  //GET BALL EQUATION (y = mx + c)
+  mb = ballYVel/ballXVel;
+  cb = ballY-(ballX*mb);
+  //console.log(mb, cb)
+
+  //GET PADDLE EQUATION (y = mx + c)
+  paddleXDiff = paddle[2]-paddle[0];
+  paddleYDiff = paddle[3]-paddle[1];
+  mp = paddleYDiff/paddleXDiff;
+  cp = paddle[1]-(paddle[0]*mp);
+  // console.log(mp, cp)
+
+  //CALCULATE INTERSECTION POINT by solving equations for x
+  xIntersect = (cp-cb)/(mb-mp)
+
+  if (paddle[0] <= paddle[2]) {
+    paddleLeft  = paddle[0]; 
+    paddleRight = paddle[2];
+  } 
+  else {
+    paddleLeft  = paddle[2]; 
+    paddleRight = paddle[0];
+  }
+
+  ballPrev = ballX-ballXVel;
+  if (ballPrev <= ballX) {
+    ballLeft = ballPrev;
+    ballRight = ballX;
+  }
+  else {
+    ballLeft = ballX;
+    ballRight = ballPrev;
+  }
+
+  if (xIntersect >= paddleLeft && xIntersect < paddleRight && xIntersect >= ballLeft && xIntersect < ballRight) {
+    console.log('paddle')
+  }
+
+  //console.log(xIntersect)
 
   background(0);
   fill(255)
